75d14946bf974ae8a065f9c0d59c36c8aa655c17
Fredrik Johansson <fredrik.johansson@gmail.com>
2012-07-18 15:37:48 +0200

    some more setup code changes

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@ from Cython.Distutils import build_ext
 
 ext_modules = [Extension("flint", ["pyflint.pyx"], libraries=["flint"])]
 
-for e  in ext_modules:
+for e in ext_modules:
     e.pyrex_directives  =  {"embedsignature":  True}
 
 setup(
   name = 'flint',
-  version = '0.1',
   cmdclass = {'build_ext':build_ext},
-  ext_modules = ext_modules
+  ext_modules = ext_modules,
+  description = 'bindings for FLINT',
+  version = '0.1',
+  url='https://github.com/fredrik-johansson/python-flint',
+  author='Fredrik Johansson',
+  author_email='fredrik.johansson@gmail.com',
+  license = 'BSD',
+  classifiers=['Topic :: Scientific/Engineering :: Mathematics']
 )
