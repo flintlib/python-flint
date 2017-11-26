177556731912edfbfac0f0fe6e163014d7bf0877
xoviat <xoviat@users.noreply.github.com>
2017-11-25 23:26:25 -0600

    [setup] use static build for win32

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
+import sys
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("flint", ["src/pyflint.pyx"], libraries=["flint", "arb"])]
+if sys.platform == 'win32':
+    libraries = ["flint", "arb", "mpir", "mpfr", "pthreads"]
+else:
+    libraries = ["flint", "arb"]
+
+ext_modules = [Extension("flint", ["src/pyflint.pyx"], libraries=libraries)]
 
 for e in ext_modules:
-    e.cython_directives  =  {"embedsignature":  True}
+    e.cython_directives = {"embedsignature": True}
 
 setup(
-  name = 'python-flint',
-  cmdclass = {'build_ext':build_ext},
-  ext_modules = ext_modules,
-  description = 'bindings for FLINT',
-  version = '0.1.2',
-  url='https://github.com/fredrik-johansson/python-flint',
-  author='Fredrik Johansson',
-  author_email='fredrik.johansson@gmail.com',
-  license = 'BSD',
-  classifiers=['Topic :: Scientific/Engineering :: Mathematics']
-)
-
+    name='python-flint',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
+    description='bindings for FLINT',
+    version='0.1.2',
+    url='https://github.com/python-flint/python-flint',
+    author='Fredrik Johansson',
+    author_email='fredrik.johansson@gmail.com',
+    license='BSD',
+    classifiers=['Topic :: Scientific/Engineering :: Mathematics'])
