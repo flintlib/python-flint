6bd3232e5b9a0a41058ad793ba3fade830fb7528
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-08-09 12:08:01 +0100

    manually modify sources.list

--- bin/cibw_before_all_linux.sh
+++ bin/cibw_before_all_linux.sh
@@ -3,6 +3,11 @@
 set -xe
 
 #yum install -y xz
+
+# The echo lines are needed for manylinux_2_24
+# https://github.com/pypa/manylinux/issues/1369#issuecomment-1546594841
+echo "deb http://archive.debian.org/debian stretch main" > /etc/apt/sources.list
+echo "deb http://archive.debian.org/debian-security stretch/updates main" >> /etc/apt/sources.list
 apt-get update
 apt-get install -y xz-utils
 
