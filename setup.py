9a3fd0bda7b58bef866e9c1b521d2d10f4142a50
giacomopope <giacomopope@gmail.com>
2023-08-18 17:02:11 +0100

    remove old pxd

--- setup.py
+++ setup.py
@@ -66,7 +66,6 @@ if os.getenv('PYTHON_FLINT_COVERAGE'):
 ext_files = [
     ("flint._flint", ["src/flint/pyflint.pyx"]), # Main Module
     # Submodules
-    # ("flint.global_context", ["src/flint/_global_context.pyx"]),
     ("flint.flint_base.flint_base", ["src/flint/flint_base/flint_base.pyx"]),
     ("flint.flint_base.flint_context", ["src/flint/flint_base/flint_context.pyx"]),
 
