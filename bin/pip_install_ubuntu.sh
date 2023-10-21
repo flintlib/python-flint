2e3e15640c4f821cc6322c295686d14ac2d87145
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-10-21 13:16:57 +0100

    Update pip install CI test

--- bin/pip_install_ubuntu.sh
+++ bin/pip_install_ubuntu.sh
@@ -19,12 +19,12 @@ sudo apt-get update
 sudo apt-get install libgmp-dev libmpfr-dev xz-utils
 
 # Only Flint 3 or newer will work.
-FLINTVER=3.0.0-alpha1
+FLINTVER=3.0.0
 
 # This will default to installing in /usr/local. If you want to install in a
 # non-standard location then configure flint with
 #    ./configure --disable-static --prefix=$PREFIX
-# If $PREFIX is no in default search paths, then at build time set
+# If $PREFIX is not in default search paths, then at build time set
 #    export C_INCLUDE_PATH=$PREFIX/include
 # and at runtime set
 #    export LD_LIBRARY_PATH=$PREFIX/lib
@@ -41,7 +41,7 @@ cd ..
 ls -l /usr/local/lib
 sudo ldconfig /usr/local/lib
 
-# Python build requirements. Ideally these would be in pyprojec.toml, but
+# Python build requirements. Ideally these would be in pyproject.toml, but
 # first need to migrate from setup.py to pyproject.toml.
 pip install 'cython>=3' numpy wheel
 
@@ -49,8 +49,8 @@ pip install 'cython>=3' numpy wheel
 echo -----------------------------------------------------------
 echo
 echo     Running:
-echo        $ pip install --no-build-isolation $1
+echo        $ pip install --no-build-isolation --no-binary $1
 echo
 echo -----------------------------------------------------------
 
-pip install --no-build-isolation $1
+pip install --no-build-isolation --no-binary $1
