c7f6aaaefc2304257f8332b71a9d7cd3fabc884f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-02-02 12:14:42 -0800

    Remove install of Python build prereqs in CIBW_BEFORE_BUILD

--- bin/cibw.sh
+++ bin/cibw.sh
@@ -15,8 +15,7 @@ export CIBW_ARCHS_MACOS="arm64"
 export CIBW_BEFORE_ALL_MACOS=bin/cibw_before_all_macosx_arm64.sh
 export CIBW_BEFORE_ALL_WINDOWS='C:\\msys64\\usr\\bin\\bash bin/cibw_before_all_windows.sh'
 
-export CIBW_BEFORE_BUILD='pip install numpy cython delvewheel'
-export CIBW_BEFORE_BUILD_WINDOWS='C:\\msys64\\usr\\bin\\bash bin/cibw_before_build_windows.sh'
+export CIBW_BEFORE_BUILD_WINDOWS='pip install delvewheel && C:\\msys64\\usr\\bin\\bash bin/cibw_before_build_windows.sh'
 
 export CIBW_TEST_COMMAND="python -m flint.test"  # override setting in pyproject.toml
 
