267e31a995993f181c79e7c89317b47ee36a55d7
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-02-02 17:37:29 +0000

    Test with flint 3.0.0 and also git main

--- bin/pip_install_ubuntu.sh
+++ bin/pip_install_ubuntu.sh
@@ -4,22 +4,38 @@ set -o errexit
 
 # This script should work to install python-flint on Ubuntu from a VCS checkout
 #
-#  $ git checkout https://github.com/flintlib/python-flint.git
+#  $ git clone https://github.com/flintlib/python-flint.git
+#  $ cd python-flint
 #  $ bin/pip_install_ubuntu.sh .
 #
 # To install an sdist from PyPI, use
 #
 #  $ bin/pip_install_ubuntu.sh python-flint
 #
+# The Flint version to build can be provided as an argument, e.g.
+#
+#  $ bin/pip_install_ubuntu.sh python-flint v3.0.1
+#
+# The version is a tag or branch in the Flint repository. If not provided, the
+# script will default to downloading the release tarball hard-coded below. Only
+# Flint 3 or newer will work.
+
+if [ -z "$2" ]; then
+    echo "Building from release tarball"
+    FLINT_GIT=""
+    FLINTVER=3.0.1
+else
+    echo "Building from git: $2"
+    FLINT_GIT=$2
+fi
+# Either . or python-flint. Passed to pip install
+PYTHON_FLINT=$1
 
 # Install runtime and build dependencies
 
 # First install their dependencies and build dependencies
 sudo apt-get update
 sudo apt-get install libgmp-dev libmpfr-dev xz-utils
-
-# Only Flint 3 or newer will work.
-FLINTVER=3.0.1
 
 # This will default to installing in /usr/local. If you want to install in a
 # non-standard location then configure flint with
@@ -29,24 +45,34 @@ FLINTVER=3.0.1
 # and at runtime set
 #    export LD_LIBRARY_PATH=$PREFIX/lib
 
-curl -O -L https://www.flintlib.org/flint-$FLINTVER.tar.gz
-tar xf flint-$FLINTVER.tar.gz
-cd flint-$FLINTVER
-  ./bootstrap.sh
-  ./configure --disable-static
-  make -j
-  sudo make install
-cd ..
+if [ -z "$FLINT_GIT" ]; then
+    # Install from release tarball
+    echo "Installing Flint $FLINTVER from release tarball"
+    curl -O -L https://www.flintlib.org/flint-$FLINTVER.tar.gz
+    tar xf flint-$FLINTVER.tar.gz
+    cd flint-$FLINTVER
+else
+    # Install from git
+    echo "Installing Flint from git: $FLINT_GIT"
+    git clone https://github.com/flintlib/flint.git
+    cd flint
+    git checkout $FLINT_GIT
+fi
+
+./bootstrap.sh
+./configure --disable-static
+make -j
+sudo make install
 
 ls -l /usr/local/lib
 sudo ldconfig /usr/local/lib
 
-# Install from checkout (or sdist).
+# Build python-flint from sdist
 echo -----------------------------------------------------------
 echo
 echo     Running:
-echo        $ pip install --no-binary :all: $1
+echo        $ pip install --no-binary :all: $PYTHON_FLINT
 echo
 echo -----------------------------------------------------------
 
-pip install --no-binary :all: $1
+pip install --no-binary :all: $PYTHON_FLINT
