fbabf32fdf2e6d7d08545c8a815e957cbb547e3c
Edgar Costa <edgarc@mit.edu>
2024-06-15 11:40:45 -0400

    fix doctests

--- src/flint/test/__main__.py
+++ src/flint/test/__main__.py
@@ -80,11 +80,14 @@ def run_doctests(verbose=None):
                flint.types.acb_series,
                flint.types.dirichlet,
                flint.functions.showgood]
-    if hasattr(flint.types, 'acb_theta'):
-        modules.append(flint.types.acb_theta)
+    try:
+        from flint.types import acb_theta
+        modules.append(acb_theta)
+    except ImportError:
+        pass
     results = [doctest.testmod(x) for x in modules]
 #    ffmpz, tfmpz = doctest.testmod(flint._fmpz, verbose=verbose)
- #   failed, total = doctest.testmod(flint.pyflint, verbose=verbose)
+#    failed, total = doctest.testmod(flint.pyflint, verbose=verbose)
     return tuple(sum(res) for res in zip(*results))
 
 
