f5a08bdc62972e06e188f0e857357ba3b94a6592
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-06-19 18:45:21 +0100

    Set strip in CFLAGS

--- bin/build_dependencies_unix.sh
+++ bin/build_dependencies_unix.sh
@@ -281,12 +281,11 @@ curl -O -L https://github.com/flintlib/flint/releases/download/v$FLINTVER/flint-
 tar xf flint-$FLINTVER.tar.gz
 cd flint-$FLINTVER
   ./bootstrap.sh
-  ./configure --prefix=$PREFIX\
+  CFLAGS='-Wl,--strip-all' ./configure --prefix=$PREFIX\
     $FLINTARB_WITHGMP\
     --with-mpfr=$PREFIX\
     --disable-static
   make -j6
-  strip libflint.so
   make install
 cd ..
 
