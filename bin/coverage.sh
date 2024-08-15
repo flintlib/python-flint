bb331cf72d0460a40eed0a178be98f34c02c4133
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-08-15 18:16:27 +0100

    Pass arguments to flint.test in bin/coverage.sh

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -8,9 +8,14 @@
 #
 #   https://github.com/cython/cython/pull/6341
 #
+# Arguments to this script are passed to python -m flint.test e.g. to skip
+# doctests and run in quiet mode:
+#
+#    bin/coverage.sh -qt
+#
 set -o errexit
 
 meson setup build -Dcoverage=true
-spin run -- coverage run -m flint.test
-coverage report -m
+spin run -- coverage run -m flint.test $@
+coverage report -m --sort=cover
 coverage html
