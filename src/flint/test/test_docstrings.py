e001635ce1e6d8b9a2d66b7b2e23bd73ae3c6745
Jake-Moss <jake.moss@uqconnect.edu.au>
2024-09-08 19:28:14 +1000

    Conditionally define the pytest doctest target

--- src/flint/test/test_docstrings.py
+++ src/flint/test/test_docstrings.py
@@ -1,7 +1,6 @@
 import doctest
 import importlib
 import pkgutil
-import pytest
 import re
 
 import flint
@@ -32,24 +31,43 @@ def find_doctests(module):
     return tests
 
 
-class PyTestDocTestRunner(doctest.DocTestRunner):
-    def report_failure(self, out, test, example, got):
-        pytest.fail(
-            "\n".join([
-                f"{test.name}, line: {test.lineno}",
-                "Failed example:",
-                f"\t{example.source.strip()}",
-                "Expected:",
-                f"\t{example.want.strip()}",
-                "Got:",
-                f"\t{got.strip()}"
-            ]),
-            pytrace=False,
-        )
+# The below definitions are only useful when pytest is a) installed, and b) being currently run.
+# We don't want to impose pytest on those that just want to use `python -m flint.test`
+try:
+    import pytest
 
+    class PyTestDocTestRunner(doctest.DocTestRunner):
+        def report_failure(self, out, test, example, got):
+            pytest.fail(
+                "\n".join([
+                    f"{test.name}, line: {test.lineno}",
+                    "Failed example:",
+                    f"\t{example.source.strip()}",
+                    "Expected:",
+                    f"\t{example.want.strip()}",
+                    "Got:",
+                    f"\t{got.strip()}"
+                ]),
+                pytrace=False,
+            )
 
-runner = PyTestDocTestRunner()
+    runner = PyTestDocTestRunner()
 
-@pytest.mark.parametrize("module,test", [(module, test) for module, test_set in find_doctests(flint) for test in test_set])
-def test_docstrings(module, test):
-    runner.run(test)
+    @pytest.mark.parametrize(
+        "test",
+        [
+            test for _, test_set in find_doctests(flint)
+            for test in test_set
+        ]
+    )
+    def test_docstrings(test):
+        runner.run(test)
+
+except ImportError:
+    class PyTestDocTestRunner(doctest.DocTestRunner):
+        pass
+
+    runner = None
+
+    def test_docstrings(module, test):
+        pass
