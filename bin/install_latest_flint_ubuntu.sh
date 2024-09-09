b5434c5ab7d630f99b68343c9c575b2d860cf9d4
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-09-09 13:56:34 +0100

    docs: use latest Flint when building docs

---
+++ bin/install_latest_flint_ubuntu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+source bin/build_variables.sh
+
+sudo apt-get update
+sudo apt-get install libgmp-dev libmpfr-dev xz-utils ninja-build
+curl -O -L https://github.com/flintlib/flint/releases/download/v$FLINTVER/flint-$FLINTVER.tar.gz
+tar xf flint-$FLINTVER.tar.gz
+tar -xzf flint-$FLINTVER.tar.gz
+cd flint-$FLINTVER && ./configure --disable-static && make -j$(expr $(nproc) + 1) && sudo make install
