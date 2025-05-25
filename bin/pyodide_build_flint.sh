80227ae7fe7e2c37cda42dcceb98e539e7dc1cc6
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-05-25 18:49:12 +0530

    Use separate scripts for better caching

---
+++ bin/pyodide_build_flint.sh
@@ -0,0 +1,21 @@
+#!usr/bin/env bash
+
+set -e
+
+curl -L https://github.com/flintlib/flint/releases/download/v3.2.2/flint-3.2.2.tar.xz -o flint-3.2.2.tar.xz
+tar -xf flint-3.2.2.tar.xz
+
+cd flint-3.2.2
+
+emconfigure ./configure \
+    --disable-dependency-tracking \
+    --disable-shared \
+    --prefix=$WASM_LIBRARY_DIR \
+    --with-gmp=$WASM_LIBRARY_DIR \
+    --with-mpfr=$WASM_LIBRARY_DIR \
+    --host=wasm32-unknown-emscripten \
+    --disable-assembly \
+    --disable-pthread
+
+emmake make -j $(nproc)
+emmake make install
