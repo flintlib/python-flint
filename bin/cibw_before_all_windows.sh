805858902e6738534c2f2e5d92563fb5799849cc
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2025-09-06 15:20:27 +0100

    maint: clean out old build-related code

--- bin/cibw_before_all_windows.sh
+++ bin/cibw_before_all_windows.sh
@@ -2,21 +2,6 @@
 
 set -o errexit
 
-# Uncomment this to run cibuildwheel locally on Windows:
-# export PATH=$PATH:/c/msys64/usr/bin:/c/msys64/mingw64/bin
-
-#
-# Make a setup.cfg to specify compiling with mingw64 (even though it says
-# mingw32...)
-#
-
-# This is not needed any more for python-flint >= 0.7.0 because meson is now
-# used as the build system rather than setuptools:
-echo '[build]' > setup.cfg
-echo 'compiler = mingw32' >> setup.cfg
-cat setup.cfg
-
-# Install the mingw-w64 toolchain and build tools
 pacman -S --noconfirm \
     mingw-w64-x86_64-gcc\
     mingw-w64-x86_64-tools-git\
@@ -28,7 +13,6 @@ pacman -S --noconfirm \
     libtool\
     #
 
-# This is slow with MinGW:
 bin/build_dependencies_unix.sh \
     --use-gmp-github-mirror\
     --patch-C23\
