acdc74b47773efb4b03b38238bb62b32f31e2f93
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2026-03-23 11:00:56 +0000

    Use msys2 tooling for gendef

--- bin/cibw_before_all_windows_amd64.sh
+++ bin/cibw_before_all_windows_amd64.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 pacman -S --noconfirm \
     mingw-w64-ucrt-x86_64-gcc\
+    mingw-w64-ucrt-x86_64-llvm-tools\
     mingw-w64-ucrt-x86_64-tools-git\
     m4\
     make\
@@ -19,28 +20,16 @@ bin/build_dependencies_unix.sh \
     --patch-C23\
     #
 
-# Assumes the standard GitHub Actions Windows 2022 runner layout.
-PATH="$PATH:$(find "/c/Program Files/Microsoft Visual Studio/2022/" -name "Hostx86")/x64/"
-
 mkdir -p .local/lib
 cd .local/bin
 for dll_file in libgmp-*.dll libmpfr-*.dll libflint*.dll
 do
-  lib_name=$(basename -s .dll ${dll_file})
-  exports_file=${lib_name}-exports.txt
+  lib_name=$(basename -s .dll "${dll_file}")
   def_file=${lib_name}.def
-  lib_file=${lib_name}.lib
-  name=$(echo ${lib_name}|sed 's/^lib//;s/[-.][0-9].*$//')
+  name=$(echo "${lib_name}" | sed 's/^lib//;s/[-.][0-9].*$//')
 
-  dumpbin //exports ${dll_file} > ${exports_file}
-
-  echo LIBRARY ${lib_name} > ${def_file}
-  echo EXPORTS >> ${def_file}
-  awk 'NR>19 && $4 != "" {print $4 " @"$1}' ${exports_file} >> ${def_file}
-  sed -i 's/$/\r/' ${def_file}
-
-  lib //def:${def_file} //out:${lib_file} //machine:x64
-  rm ${exports_file} ${def_file} ${lib_name}.exp
-  mv ${lib_file} ../lib/${name}.lib
+  gendef "${dll_file}"
+  llvm-lib /def:"${def_file}" /out:"../lib/${name}.lib" /machine:x64 /nologo
+  rm "${def_file}"
 done
 cd ../..
