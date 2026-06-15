46c3bac7fd3addf0909ab768475efe579ac53337
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2026-06-15 14:48:17 +0200

    Use FLINT main branch in doctest

--- bin/install_latest_flint_ubuntu.sh
+++ bin/install_latest_flint_ubuntu.sh
@@ -7,9 +7,8 @@ source bin/build_variables.sh
 sudo apt-get update
 sudo apt-get install libgmp-dev libmpfr-dev xz-utils ninja-build
 
-curl -O -L https://github.com/flintlib/flint/releases/download/v$FLINTVER/flint-$FLINTVER.tar.gz
-tar -xzf flint-$FLINTVER.tar.gz
-cd flint-$FLINTVER && ./configure --disable-static && make -j$(expr $(nproc) + 1) && sudo make install
+git clone https://github.com/flintlib/flint
+cd flint && ./bootstrap.sh && ./configure --disable-static && make -j$(expr $(nproc) + 1) && sudo make install
 
 ls -l /usr/local/lib
 sudo ldconfig /usr/local/lib
