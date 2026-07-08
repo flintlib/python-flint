edb2242ebff90aa823ef62dfa12c83753d35c31e
Madhu18S <PES1UG24AM316@pes.edu>
2026-07-08 23:14:51 +0530

    style: fix trailing whitespaces and newlines for cython-lint compliance

--- src/flint/types/qqbar.pxd
+++ src/flint/types/qqbar.pxd
@@ -1,9 +1,6 @@
-# src/flint/types/qqbar.pxd
-from flint.flint_base.flint_base cimport flint_ctx, flint_scalar
 from flint.flintlib.types.flint cimport slong
 from flint.flintlib.functions.fmpz cimport fmpz_t
 from flint.flintlib.functions.fmpq cimport fmpq_t
-
 
 cdef extern from "flint/qqbar.h" nogil:
     ctypedef struct qqbar_struct:
@@ -15,7 +12,7 @@ cdef extern from "flint/qqbar.h" nogil:
     void qqbar_clear(qqbar_t res)
     void qqbar_set(qqbar_t res, const qqbar_t x)
     void qqbar_set_si(qqbar_t res, slong x)
-    
+
     # Printing methods
     void qqbar_print(const qqbar_t x)
     void qqbar_printn(const qqbar_t x, slong n)
@@ -29,6 +26,8 @@ cdef extern from "flint/qqbar.h" nogil:
     void qqbar_sub(qqbar_t res, const qqbar_t x, const qqbar_t y)
     void qqbar_mul(qqbar_t res, const qqbar_t x, const qqbar_t y)
     void qqbar_div(qqbar_t res, const qqbar_t x, const qqbar_t y)
+
+from flint.flint_base.flint_base cimport flint_scalar
 
 cdef class qqbar(flint_scalar):
     cdef qqbar_t val
--- src/flint/types/qqbar.pyx
+++ src/flint/types/qqbar.pyx
@@ -1,15 +1,22 @@
-# src/flint/types/qqbar.pyx
-from flint.flint_base.flint_base cimport flint_scalar
+from flint.flintlib.functions.qqbar cimport (
+    qqbar_init,
+    qqbar_clear,
+    qqbar_set,
+    qqbar_set_si,
+    qqbar_printn,
+    qqbar_set_fmpz,
+    qqbar_set_fmpq,
+    qqbar_add,
+    qqbar_sub,
+    qqbar_mul,
+    qqbar_div
+)
 from flint.types.fmpz cimport fmpz
 from flint.types.fmpq cimport fmpq
-from flint.types.qqbar cimport (
-    qqbar_init, qqbar_clear, qqbar_set, qqbar_set_si,
-    qqbar_add, qqbar_sub, qqbar_mul, qqbar_div, qqbar
-)
 
 cdef class qqbar(flint_scalar):
     """
-    The qqbar class represents algebraic numbers inside python-flint.
+    The qqbar class represents algebraic numbers.
     """
 
     def __cinit__(self):
@@ -31,7 +38,38 @@ cdef class qqbar(flint_scalar):
             else:
                 raise TypeError("Cannot initialize qqbar type from input")
 
-    # Explicit Python Operator Overloads
+    def str(self, Py_ssize_t digits=15):
+        """
+        Converts the algebraic number to a decimal string representation by capturing C-level stdout.
+        """
+        import os
+        import sys
+
+        cdef int stdout_fd = 1
+        cdef int saved_stdout = os.dup(stdout_fd)
+
+        pipe_read, pipe_write = os.pipe()
+        os.dup2(pipe_write, stdout_fd)
+
+        try:
+            qqbar_printn(self.val, digits)
+            sys.stdout.flush()
+        finally:
+            os.close(pipe_write)
+            os.dup2(saved_stdout, stdout_fd)
+            os.close(saved_stdout)
+
+        cdef bytes captured = os.read(pipe_read, 4096)
+        os.close(pipe_read)
+
+        return captured.decode('utf-8').strip()
+
+    def __str__(self):
+        return self.str()
+
+    def __repr__(self):
+        return f"qqbar({self.str()})"
+
     def __add__(x, y):
         cdef qqbar res = qqbar()
         cdef qqbar c_x = x if isinstance(x, qqbar) else qqbar(x)
@@ -59,42 +97,3 @@ cdef class qqbar(flint_scalar):
         cdef qqbar c_y = y if isinstance(y, qqbar) else qqbar(y)
         qqbar_div(res.val, c_x.val, c_y.val)
         return res
-    def str(self, Py_ssize_t digits=15):
-        """
-        Converts the algebraic number to a decimal string representation by capturing C-level stdout.
-        """
-        import os
-        import sys
-
-        # Save current stdout file descriptor
-        cdef int stdout_fd = 1
-        cdef int saved_stdout = os.dup(stdout_fd)
-        
-        # Create a pipe to catch stream output
-        pipe_read, pipe_write = os.pipe()
-        
-        # Redirect stdout to our pipe
-        os.dup2(pipe_write, stdout_fd)
-        
-        try:
-            # Execute raw C-level printing function
-            qqbar_printn(self.val, digits)
-            sys.stdout.flush()
-        finally:
-            # Revert stdout back to original descriptor
-            os.close(pipe_write)
-            os.dup2(saved_stdout, stdout_fd)
-            os.close(saved_stdout)
-            
-        # Read the captured string from the pipe
-        cdef bytes captured = os.read(pipe_read, 4096)
-        os.close(pipe_read)
-        
-        return captured.decode('utf-8').strip()
-
-    def __str__(self):
-        return self.str()
-
-    def __repr__(self):
-        # Displays a clean numerical representation
-        return f"qqbar({self.str()})"
